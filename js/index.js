86191666f808f82c3df7f6b7547fae3b7618acd9
Ignacio Torres <103877002+torresignacio97@users.noreply.github.com>
2022-10-17 07:03:37 -0300

    Proyecto terminado

--- js/index.js
+++ js/index.js
@@ -1,13 +1,69 @@
 window.sr = ScrollReveal();
 
-    sr.reveal('.navbar', {
+    sr.reveal('.navbarra', {
         duration: 2000,
         origin: "bottom",
         distance: "-100px"
     });
 
-    sr.reveal('botonesRrss', {
+    sr.reveal('.barraBusqueda', {
         duration: 2000,
         origin: "bottom",
         distance: "-100px"
     });
+
+    sr.reveal('.logotipoOptica', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "-100px"
+    });
+
+    sr.reveal('.botonesRedes', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "-100px"
+    })
+
+    sr.reveal('.slogan-titulo', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "-100px"
+    })
+
+    sr.reveal('.slogan-texto', {
+        duration: 2000,
+        origin: "top",
+        distance: "-100px"
+    })
+
+    sr.reveal('.novedades', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "-100px"
+    })
+
+    sr.reveal('.botonesRedes', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "-100px"
+    })
+
+    sr.reveal('.container', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "-100px"
+    })
+
+    sr.reveal('.rrss-reveal', {
+        duration: 2000,
+        origin: "end",
+        distance: "-100px"
+    })
+
+    sr.reveal('.contacto-reveal', {
+        duration: 2000,
+        origin: "start",
+        distance: "100px"
+    })
+
+    
